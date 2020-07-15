8d35810450ed6e1c4ec878d27f5bfdc97c6043d1
Tatyana Kutalo <tatyana@kutalo.com>
2020-07-15 13:32:36 +0300

    adding SubscriptionListComponent to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { CartComponent } from './cart/cart.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { SubscriptionListComponent } from './subscription-list/subscription-list.component';
 
 const routes: Routes = [
     { path: '', component: ProductListComponent },
     { path: 'orders', component: OrderListComponent },
     { path: 'orders/:id', component: OrderDetailComponent },
+    { path: 'subscriptions', component: SubscriptionListComponent },
     { path: 'cart', component: CartComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
